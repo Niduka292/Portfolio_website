3842d27db9575763a6b2eafe71a1a3cf26f66bd2
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2025-10-25 15:21:46 +0530

    Designed the structure of projects section

--- frontend/js/components/Projects.js
+++ frontend/js/components/Projects.js
@@ -1,7 +1,7 @@
 const Projects = {
 
     template : `
-        <div class="projects">
+        <div class="projects-section">
             <div class="section-title">
                 <p>Portfolio Showcase</p>
             </div>
@@ -10,7 +10,99 @@ const Projects = {
                     A glimpse into the projects I’ve built where ideas meet code to create functional, user-centered web experiences.
                 </p>
             </div>
+
+            <div class="projects">
+                <div class="projects-title-div">
+                    <div class="projects-header-1">
+                            <p><></p>
+                        </div>
+                        <div class="projects-header-2">
+                            <p>Projects</p>
+                        </div>
+                </div>
+                <div class="project-cards-container">
+                    <div class="project-cards-row">
+                        <div class="project-card-1">
+                            <div class="project-image">
+                                <img src="./assets/project-1-pic.png"></img>
+                            </div>
+                            <div class="project-text">
+                                <div class="project-title">
+                                    <p>Airplane Ticket Booking System</p>
+                                </div>
+                                <div class="project-description">
+                                    <p>
+                                        Desktop-based Airplane Ticket Booking Management System developed using JavaFX, Java and PostgreSQL.
+                                    </p>
+                                </div>
+                                <a class="view-details">View Details &#8594;</a>
+                            </div>
+                        </div>
+                        <div class="project-card-2">
+                            <div class="project-image">
+                                <img src="./assets/project-2-pic.png"></img>
+                            </div>
+                            <div class="project-text">
+                                <div class="project-title">
+                                    <p>Online Food Ordering System</p>
+                                </div>
+                                <div class="project-description">
+                                    <p>
+                                        A simplified Online Food Ordering System designed using Java as an assignment.
+                                    </p><br>
+                                </div>
+                                <a class="view-details">View Details &#8594;</a>
+                            </div>
+                        </div>
+                        <div class="project-card-3">
+                            <div class="project-image">
+                                <img src="./assets/project-3-pic.png"></img>
+                            </div>
+                            <div class="project-text">
+                                <div class="project-title">
+                                    <p>To Do List</p>
+                                </div>
+                                <div class="project-description">
+                                    <p>
+                                        Simple to-do list application built using Javascript and css as a learning project. 
+                                    </p><br>
+                                </div>
+                                <a class="view-details">View Details &#8594;</a>
+                            </div>  
+                        </div>                      
+                    </div>
+                    <div class="project-cards-row">
+                        <div class="project-card-1">
+                            <div class="project-image">
+                                <img src="./assets/project-4-pic.png"></img>
+                            </div>
+                            <div class="project-text">
+                                <div class="project-title">
+                                    <p>Email Application</p>
+                                </div>
+                                <div class="project-description">
+                                    <p>A simple web application with functionality to do basic operations as an email application
+                                    </p><br>
+                                </div>
+                                <a class="view-details">View Details &#8594;</a>
+                            </div>
+                        </div>                     
+                    </div>
+
+                </div>
+            </div><br>
+            <div class=tech-stack>
+                <div class="projects-title-div">
+                    <div class="projects-header-1">
+                        <p>&#128230;</p>
+                    </div>
+                    <div class="projects-header-2">
+                        <p>Teck Stack</p>
+                    </div>
+                </div>
+            </div>
         </div>
+        
         
     `
 }
