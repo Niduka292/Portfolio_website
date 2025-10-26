f2f68c1e8ccfee1351fc5217df813b1319900811
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2025-10-26 11:16:45 +0530

    Finished the basic design of connect with me component

--- frontend/js/components/Contact.js
+++ frontend/js/components/Contact.js
@@ -15,16 +15,53 @@ const Contact = {
                     </div>
                     <div class="connects-container">
                         <div class="connect-linkedin">
-                            <a class="linkedin-btn-image" href="https://linkedin.com/in/niduka-jayathunga"><img src="./assets/linkedin-animated.png"/> 
-                                <p class="connect-component-header">Let's Connect</p>
-                                <p class="connect-component-body"> on LinkedIn</p>
+                            <a class="linkedin-btn" href="https://linkedin.com/in/niduka-jayathunga" target="_blank"> 
+
+                                <div class="connect-with-me-component">
+                                    <img src="./assets/linkedin-animated.png" class="linkedin-icon"/>
+                                    <div class="connect-with-me-text">
+                                        <p class="connect-component-line-1">
+                                            Let's Connect
+                                        </p>
+                                        <p class="connect-component-line-2">
+                                            On Linkedin
+                                        </p>
+                                    </div>
+                                </div>
                             </a>
                         </div>
-                        <div class="connect-component">
-                            
+
+                        <div class="connect-insta">
+                            <a class="linkedin-btn" href="https://www.instagram.com/niduka_065_" target="_blank"> 
+
+                                <div class="connect-with-me-component">
+                                    <img src="./assets/insta-animated.png" class="linkedin-icon"/>
+                                    <div class="connect-with-me-text">
+                                        <p class="connect-component-line-1">
+                                            Instagram
+                                        </p>
+                                        <p class="connect-component-line-2">
+                                            @niduka_065_
+                                        </p>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
-                        <div class="connect-component">
-                            
+                        <div class="connect-github">
+                            <a class="linkedin-btn" href="https://github.com/Niduka292" target="_blank"> 
+
+                                <div class="connect-with-me-component">
+                                    <img src="./assets/github-animated.png" class="linkedin-icon"/>
+                                    <div class="connect-with-me-text">
+                                        <p class="connect-component-line-1">
+                                            Github
+                                        </p>
+                                        <p class="connect-component-line-2">
+                                            @Niduka292
+                                        </p>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
