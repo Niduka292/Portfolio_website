f7efa1e98549ee4c969fc7696e62c4464e6fa721
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2025-11-01 01:20:12 +0530

    Added pages for projects with more details

--- frontend/js/components/projects-data.js
+++ frontend/js/components/projects-data.js
@@ -2,31 +2,25 @@ const projectsData = [
   {
     title: "Airplane Ticket Booking System",
     description: "Desktop-based Airplane Ticket Booking Management System developed using JavaFX, Java and PostgreSQL.",
-    img: "./assets/project-1-pic.png",
+    img: "./assets/airplane-ticket-booking.png",
     link: "/projects/airplane-ticket-booking.html"
   },
   {
     title: "Online Food Ordering System",
     description: "A simplified Online Food Ordering System designed using Java as an assignment.",
-    img: "./assets/project-2-pic.png",
+    img: "./assets/online-food-ordering.png",
     link: "/projects/online-food-ordering.html"
   },
   {
     title: "Wine Analysis",
     description: "Data analysing and visualization project built using Python, NLP and Dash.",
-    img: "./assets/project-3-pic.png",
+    img: "./assets/wine-analytics.png",
     link: "/projects/wine-analysis.html"
-  },
-  {
-    title: "Email Application",
-    description: "A simple web application with functionality to do basic operations as an email application.",
-    img: "./assets/project-4-pic.png",
-    link: "/projects/email-application.html"
   },
   {
     title: "To Do List",
     description: "Simple to-do list application built using Javascript and CSS as a learning project.",
-    img: "./assets/project-5-pic.png",
+    img: "./assets/to-do-list.png",
     link: "/projects/to-do-list.html"
   }
 ];
