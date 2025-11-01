e9aa1fcd0464160512dd9a5c9e36979cf4f84f99
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2025-11-01 09:48:09 +0530

    Updated app.js

--- backend/app.js
+++ backend/app.js
@@ -8,34 +8,25 @@ import contactRoutes from "./routes/contact.js";
 
 dotenv.config();
 
-const PORT = 3000;
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-var app = express();
+const app = express();
+
 app.use(express.json());
 app.use(cors());
-app.use("/contact",contactRoutes);
+app.use("/contact", contactRoutes);
 
 // Serve static frontend
 app.use(express.static(path.join(__dirname, "../frontend")));
 
-// Serve default route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
-.then(function(){
-    console.log("MongoDB connected successfully");
-})
-.catch(function(err){
-    console.log("Failed to connected to the MongoDB: ",err);
-});
-
-app.get("/", function(req,res){
-    res.send("<h1>Express application</h1>")
-});
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.log("MongoDB connection error:", err));
 
 export default app;
